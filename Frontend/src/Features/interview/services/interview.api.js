e0b216c73027c71830aa2a5f53fd6c1e21723f3e
ubaid <loneubaid251@gmail.com>
2026-07-04 10:17:11 +0530

    Use environment variable for API URLs

--- Frontend/src/Features/interview/services/interview.api.js
+++ Frontend/src/Features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/resume",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/resume`,
   withCredentials: true,
 });
 
